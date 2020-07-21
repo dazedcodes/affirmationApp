cfee52e9bb08553769902710a1dea5d910c67c5e
Maya Murphy <murphym2@carleton.edu>
2020-07-21 08:11:09 -0500

    failed response errors occuring

--- app.js
+++ app.js
@@ -7,16 +7,20 @@ document.getElementById("btn-affirm").addEventListener("click", (e) => {
     const aCard = aElement.appendChild(aCardElement);
     aCard.setAttribute("class", "card bg-dark");
 
-    // const url = "https://www.affirmations.dev/";
-    // fetch(url).then((response) => {
-    //     response.json().then((data) => {
-    //         console.log("Data", data);
-    //         const keys = Object.keys(data);
-    //         console.log("Data Keys", keys);
-    //     })
-    //     .catch((err) => console.log(err));
-    // })
-    // .catch((err) => console.log(err));
+    const url = "https://www.affirmations.dev/";
+    fetch(url, {
+        // headers: {
+        //     "Content-Type": "application/json",
+        //     "Access-Control-Allow-Origin": "https://www.affirmations.dev/",
+        // }
+    }).then((data) => {
+
+        console.log("Data", data);
+        // const keys = Object.keys(data);
+        // console.log("Data Keys", keys);
+    })
+        .catch((err) => console.log(err));
+
 
     const randomAffirmation = "You are a capable human.";
     const aCardBody = aCardElement.appendChild(aCardBodyElement);
